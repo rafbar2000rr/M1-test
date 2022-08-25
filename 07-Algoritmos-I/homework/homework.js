a229f27d5aa9ffca65d471d5e78ee539269bb3b7
Rafael Ibarra <rafbar2000rr@gmail.com>
2022-08-25 13:08:24 -0500

    commit

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let array=[1];
+  let i=2;
+  
+  while(num!==1){
+    if(num % i ===0){
+      array.push(i);
+      num=num/i;
+    }else{
+      i++;
+    }
+    
+  }
+return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +26,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let swap=true;
+  while(swap){
+    swap=false;
+    for(let i=0;i<array.length-1;i++){
+      if(array[i]>array[i+1]){
+        let aux=array[i];
+        array[i]=array[i+1];
+        array[i+1]=aux;
+        swap=true;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +47,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i=1;i<array.length;i++){
+    let j=i-1;
+    let aux=array[i];
+    while(j>=0 && array[j]>aux){
+      array[j+1]=array[j];
+      j--;
+   }
+    array[j+1]=aux;
+  }
+  return array;
 }
 
 
@@ -32,7 +65,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i=0; i<array.length-1;i++){
+    let min=i;
+    for(let j=i+1;j<array.length;j++){
+      if(array[j]<array[min]){
+        min=j;
+      }
+    }
+    let aux=array[i];
+    array[i]=array[min];
+    array[min]=aux;
+  }
+  return array;
 }
 
 
